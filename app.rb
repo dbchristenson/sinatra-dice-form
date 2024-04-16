8fcd46491e5fc5f314d98186e1755bd030e516ee
DB Christenson <dbeauchristenson@gmail.com>
2024-04-16 08:10:48 +0000

    functionality achieved

--- app.rb
+++ app.rb
@@ -2,8 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:form)
+end
+
+get("/process_roll") do
+  @number_of_dice = (params['dice'] || 1).to_i
+  @number_of_sides = (params['sides'] || 6).to_i
+  @rolls = []
+
+  @number_of_dice.times do
+    die = rand(1..@number_of_sides)
+    @rolls.push(die)
+  end
+
+  erb(:process_rolls)
 end
